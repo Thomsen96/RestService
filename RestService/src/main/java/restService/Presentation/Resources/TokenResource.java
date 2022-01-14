08ee08886378efc963daa18b13198ba4d23f739d
Thomsen96 <jonastoftethomsen@gmail.com>
2022-01-14 22:45:39 +0100

    End to rest service connection

--- RestService/src/main/java/restService/Presentation/Resources/TokenResource.java
+++ RestService/src/main/java/restService/Presentation/Resources/TokenResource.java
@@ -1,13 +1,12 @@
 package restService.Presentation.Resources;
 
 import messaging.MessageQueue;
-import restService.Domain.Token;
+import messaging.implementations.RabbitMqQueue;
 import restService.Resources.TokenService;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.concurrent.CompletableFuture;
 
 @Path("/tokens")
 public class TokenResource {
@@ -17,17 +16,21 @@ public class TokenResource {
     // deregister
     // getTokens
     // getReport
-    public MessageQueue mq;
-    CustomerMessageService cms = new CustomerMessageService(mq);
-    private CompletableFuture<String> sessionHandled;
+    //public MessageQueue mq = new RabbitMqQueue("localhost");
+    //CustomerMessageService cms = new CustomerMessageService(mq);
 
-    TokenService tsr = new TokenService(mq);
+    //TokenService tsr = new TokenService(mq);
 
     @POST
     @Path("{customerId}/{numberOfTokens}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getTokens(@PathParam("customerId") String customerId, @PathParam("numberOfTokens") Integer numberOfTokens) {
-        var token = tsr.getTokensMessageSerivce(customerId, numberOfTokens).getArgument(0, Token.class);
         return Response.status(Response.Status.CREATED).entity(String.format("Hello %s! Here are %2d tokens!", customerId, numberOfTokens)).build();
+    }
+
+    @GET
+    @Path("/status")
+    public Response get() {
+        return Response.status(Response.Status.OK).entity(new String("OK")).build();
     }
 }
