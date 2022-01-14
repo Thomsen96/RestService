68312441bf41d11f0b07cec94d20a37296790c56
Nicholas <36410886+GodOfAllBeings@users.noreply.github.com>
2022-01-14 12:35:21 +0100

    Get tokens with completeableFuture and sessionID

--- RestService/src/main/java/restService/Presentation/Resources/CustomerResource.java
+++ RestService/src/main/java/restService/Presentation/Resources/CustomerResource.java
@@ -1,8 +1,14 @@
 package restService.Presentation.Resources;
 
+import messaging.Event;
+import messaging.MessageQueue;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 
 @Path("/customers")
 public class CustomerResource  {
@@ -14,24 +20,32 @@ public class CustomerResource  {
     // deregister
     // getTokens
     // getReport
-	
+	private MessageQueue mq;
+	CustomerMessageService cms = new CustomerMessageService(mq);
+	private CompletableFuture<String> sessionHandled = new CompletableFuture<>();
 //	private static TokenService service = new TokenService(new LocalTokenRepository());
 
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String hello() {
-		return "Hello from customer root";
+	private String addFuture(String sessionId){
+		new Thread(() -> {
+			this.sessionHandled.complete(sessionId);
+		}).start();
+		return sessionHandled.join();
 	}
 	
 	@GET
 	@Path("{customerId}/{numberOfTokens}")
-	@Produces(MediaType.APPLICATION_JSON)    
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response getTokens(@PathParam("customerId") String customerId, @PathParam("numberOfTokens") Integer numberOfTokens) {
-//		return Response.status(Response.Status.CREATED).entity(service.createTokens(numberOfTokens,customerId)).build();
+		String sessionID = UUID.randomUUID().toString();
+		Event e = new Event("CustomerVerified", new Object[]{customerId, sessionID});
+		addFuture(sessionID);
+		mq.publish(e);
 		return Response.status(Response.Status.CREATED)
 				.entity(String.format("Hello %s! Here are %2d tokens!", customerId, numberOfTokens))
 				.build();
 	}
+
+
 
 //	@POST
 //	@Path("{customerId}/{numberOfTokens}")
