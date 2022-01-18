e16ad66b27f9213febcb9dffe9842d1157ead914
Uniel <adam@saunte.dk>
2022-01-18 17:53:28 +0100

    Begun refactoring of account services

--- RestService/src/test/java/dtu/services/CustomerEventsSteps.java
+++ RestService/src/test/java/dtu/services/CustomerEventsSteps.java
@@ -11,6 +11,7 @@ import io.cucumber.java.en.*;
 import messaging.Event;
 import messaging.MessageQueue;
 import restService.Application.AccountService;
+import restService.Application.AccountService.Role;
 
 public class CustomerEventsSteps {
 
@@ -18,11 +19,9 @@ public class CustomerEventsSteps {
 	
 	AccountService accountService = new AccountService(messageQueue);
 	
-	
 	CompletableFuture<String> result = new CompletableFuture<String>();
 	CompletableFuture<String> result2 = new CompletableFuture<String>();
 	
-//	List<Thread> threads = new List<Thread>();
 	
 	String accountNumber;
 	String sessionId;
@@ -31,27 +30,9 @@ public class CustomerEventsSteps {
 	String accountNumber2;
 	String sessionId2;
 	String customerId2;
-
 	
-//	public void aaa() {
-//		// Data from REST
-//		String sessionID = UUID.randomUUID().toString();
-//		String accountNo = UUID.randomUUID().toString();
-//		// Invoke AccountService
-//		String response = accountService.createCustomerCreationRequest(sessionID, accountNo);
-//		// Create a thread to simulate response
-//		var thread = new Thread(() -> {
-//			Event e = new Event(AccountService.CUSTOMER_CREATION_RESPONSE, new Object[] {"71234781"});
-//			accountService.customerCreationResponseHandler(sessionId, e);
-//		});
-//		thread.start();
-//	}
-//	
-//	@Then("a {string} event with the account number and a sessionId has been published to the message queue")
-//	public void anEventWithTheAccountNumberAndASessionIdHasBeenPublishedToTheMessageQueue(String eventType) {
-//	    Event e = new Event(eventType, new Object[] {accountNumber, sessionId});
-//	    verify(messageQueue).publish(e);
-//	}
+	Role role;
+
 	
 	@Given("a new createCustomer session has started with accountId {string}")
 	public void aNewCreateCustomerSessionHasStartedWithAccountId(String accountNumber) {
@@ -68,7 +49,7 @@ public class CustomerEventsSteps {
 	@When("the message is sent")
 	public void theMessageIsSent() {
 		var thread = new Thread(() -> {
-			customerId = accountService.createCustomerCreationRequest(sessionId, accountNumber);
+			customerId = accountService.createCustomerCreationRequest(sessionId, accountNumber, Role.CUSTOMER);
 		});
 		thread.start();
 	}
@@ -87,10 +68,10 @@ public class CustomerEventsSteps {
 	@When("the messages are sent at the same time")
 	public void theMessagesAreSentAtTheSameTime() {
 		var thread1 = new Thread(() -> {
-			customerId = accountService.createCustomerCreationRequest(sessionId, accountNumber);
+			customerId = accountService.createCustomerCreationRequest(sessionId, accountNumber, Role.CUSTOMER);
 		});
 		var thread2 = new Thread(() -> {
-			customerId2 = accountService.createCustomerCreationRequest(sessionId2, accountNumber2);
+			customerId2 = accountService.createCustomerCreationRequest(sessionId2, accountNumber2, Role.CUSTOMER);
 		});
 		thread1.start();
 		thread2.start();
