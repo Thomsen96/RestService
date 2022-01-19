30c12bd8a07ccdb474e27ada54693fd79182ebc2
Uniel <adam@saunte.dk>
2022-01-19 12:23:16 +0100

    Refactored /merchant/payment getStatus logic to service

--- RestService/src/main/java/restService/Presentation/ManagerRessource.java
+++ RestService/src/main/java/restService/Presentation/ManagerRessource.java
@@ -10,18 +10,16 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-
 @Path("/manager")
 public class ManagerRessource {
-	
+
   @GET
   @Path("/reports")
   public Response getReport() {
-			return Response
-					.status(Response.Status.OK)
-					.entity(String.format("Report requested for manager"))
-					.build();
+    return Response
+        .status(Response.Status.OK)
+        .entity(String.format("Report requested for manager"))
+        .build();
   }
-	
 
 }
